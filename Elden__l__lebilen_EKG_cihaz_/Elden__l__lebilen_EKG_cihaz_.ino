6bf4b4df98a9756788d9369426d01e4676cb3a33
metinakkin <metin_akkin@hotmail.com>
2016-12-10 16:13:30 +0200

    Add files via upload

---
+++ Elden__l__lebilen_EKG_cihaz_/Elden__l__lebilen_EKG_cihaz_.ino
@@ -0,0 +1,28 @@
+int led=3;
+int nabizsensor=A0;
+double alpha=0.75;
+int periyot=20;
+double degisim=0.0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+
+}
+
+void loop()
+{
+  static double eskideger=0;
+  static double eskidegisim=0;
+  int hamdeger=analogRead(nabizsensor);
+  double deger= alpha*eskideger+(1-alpha)*hamdeger;
+  degisim=deger-eskideger;
+  Serial.println(degisim);
+
+  digitalWrite(led,(degisim<-5.0 && eskidegisim>5.0));
+  eskideger=deger;
+  eskidegisim=degisim;
+  delay(periyot);
+
+}
